878e30df182c6a57e87e79aafc19fa8dca72ca6f
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-12-30 16:51:09 +0200

    chore: init the project

---
+++ src/js/controller.js
@@ -0,0 +1,48 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+const API_KEY = '198a1e55-d991-4de3-8119-97018d0e7609';
+const BASE_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+const url = `${BASE_URL}?search=pizza`;
+console.log(url);
+console.log(url);
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+
+async function getRecipe() {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+getRecipe();
